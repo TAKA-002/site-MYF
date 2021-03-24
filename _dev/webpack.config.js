b2438f01e7a9789b923a07f33e2be0b4a657c5b4
taka <6015abc@gmail.com>
2021-03-25 03:29:28 +0900

    inicial commit

---
+++ _dev/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "../js"),
+    filename: "main.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "../css/style.css" })],
+};
