3b78cf38a1da8c455851f6280db81ffe4b74cea7
taka <6015abc@gmail.com>
2021-05-16 01:42:32 +0900

    【更新】第２層のテンプレート化

---
+++ project/_dev/src/modules/js/common/common_parts.js
@@ -0,0 +1,101 @@
+//==================================================
+//ヘッダーの共通部分を呼び出す
+//==================================================
+
+//=============
+//ヘッダー
+//=============
+export function header() {
+  $.ajax({
+    url: "/site-MYF/includes/mod_header.html",
+    cache: false,
+    dataType: "html",
+    success: function (data) {
+      $("#header").html(data);
+    },
+    // error: function (data) {
+    //   $("#header").text("えらー");
+    // },
+  });
+}
+
+//=============
+//フッダー
+//=============
+export function footer() {
+  $.ajax({
+    url: "/site-MYF/includes/mod_footer.html",
+    cache: false,
+    dataType: "html",
+    success: function (data) {
+      $("#footer").html(data);
+    },
+    // error: function (data) {
+    //   $("#footer").text("えらー");
+    // },
+  });
+}
+
+//=============
+//トップへ戻るボタン
+//=============
+export function toTopBtn() {
+  $(function () {
+    var toPageTopBtn = $("#toPageTop");
+    toPageTopBtn.hide();
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        toPageTopBtn.fadeIn();
+      } else {
+        toPageTopBtn.fadeOut();
+      }
+    });
+
+    toPageTopBtn.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+    });
+  });
+}
+
+//=============
+//menuボタン
+//=============
+export function menuBtn() {
+  $(function () {
+    $(document).on("click", "#menu__btn", function () {
+      console.log("クリックはされている");
+
+      // #menu_btnに.activeがあるかどうか;
+      if ($(this).hasClass("active")) {
+        // .activeがあるときの処理
+        $(this).removeClass("active");
+        $(this).text("MENU");
+        $("#menu").removeClass("open");
+        $(".menu__background").removeClass("open");
+      } else {
+        // .activeがないときの処理
+        $(this).addClass("active");
+        $(this).text("CLOSE");
+        $("#menu").addClass("open");
+        $(".menu__background").addClass("open");
+      }
+    });
+
+    // メニューの背景がクリックされたときの処理
+    $(document).on("click", ".menu__background", function () {
+      // .menu-backgroundに.openがあるかどうか
+      if ($(this).hasClass("open")) {
+        // .openがあるときの処理
+        $(this).removeClass("open");
+        $("#menu__btn").removeClass("active").text("MENU");
+        $("#menu").removeClass("open");
+      }
+    });
+  });
+}
