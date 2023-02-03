17062eabddcdfcc4b0e3df93252859fc03ec6498
Takayuki Miura <6015abc@gmail.com>
2023-02-03 22:38:43 +0900

    2021日本

--- about-us/report/2021/js/chart_01.js
+++ about-us/report/2021/js/chart_01.js
@@ -1,35 +1,36 @@
-Highcharts.chart('chart_01', {
+Highcharts.chart("chart_01", {
   chart: {
-    type: 'pie'
+    type: "pie",
   },
   title: {
-    text: '団体運営費 支出割合グラフ 2021'
+    text: "団体運営費 支出割合グラフ 2021",
   },
   subtitle: {
-    text: ''
+    text: "",
   },
 
   accessibility: {
     announceNewData: {
-      enabled: true
+      enabled: true,
     },
     point: {
-      valueSuffix: '%'
-    }
+      valueSuffix: "%",
+    },
   },
 
   plotOptions: {
     series: {
       dataLabels: {
         enabled: true,
-        format: '{point.name}: {point.y:.1f}%'
-      }
-    }
+        format: "{point.name}: {point.y:.1f}%",
+      },
+    },
   },
 
   tooltip: {
     headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    pointFormat:
+      '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>',
   },
 
   series: [
@@ -38,42 +39,30 @@ Highcharts.chart('chart_01', {
       colorByPoint: true,
       data: [
         {
-          name: "日本募金活動費",
-          y: 20.46,
+          name: "給食プログラムサポート",
+          y: 36.82,
         },
         {
           name: "事務所賃貸費用",
-          y: 15.02,
+          y: 13.67,
         },
         {
-          name: "募金活動使用機材購入費",
-          y: 13.49,
+          name: "寄付品の海外発送料",
+          y: 13.04,
         },
         {
-          name: "現地学校トイレ施設建設費",
-          y: 12.98,
+          name: "奨学金",
+          y: 7.36,
         },
         {
-          name: "学生奨学金",
-          y: 11.20,
-        },
-        {
-          name: "現地学校スタッフ給与",
-          y: 10.87,
-        },
-        {
-          name: "保険関連費",
-          y: 4.83,
-        },
-        {
-          name: "インターネットサーバー・通信関連費",
-          y: 3.86,
+          name: "トイレ・図書室・パソコン室建設",
+          y: 5.29,
         },
         {
           name: "その他",
-          y: 6.94,
-        }
-      ]
-    }
+          y: 23.82,
+        },
+      ],
+    },
   ],
 });
